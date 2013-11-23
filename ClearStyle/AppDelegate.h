63be107bcaecafd54542096b7f0d328f45ec6fb3
Tom Bell <tom.bell.code@gmail.com>
2013-11-23 13:23:36 +0100

    Initial Commit

---
+++ ClearStyle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClearStyle
+//
+//  Created by Tom Bell on 23/11/2013.
+//  Copyright (c) 2013 Tom Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClearStyle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ClearStyle
+//
+//  Created by Tom Bell on 23/11/2013.
+//  Copyright (c) 2013 Tom Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
